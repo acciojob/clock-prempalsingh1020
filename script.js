452b8b6fa9cfa8173eb7b89f1b55b2ec3061609e
prempalsingh1020 <prempalsingh030499@gmail.com>
2026-03-15 12:58:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+
+
+let para  = document.querySelector("#timer");
+
+
+let time_date = new Date();
+
+function  ClockTimer(){
+
+	let str = ""
+	let h = time_date.getHours();
+	let m = time_date.getMinutes();
+	let s = time_date.getSeconds();
+
+	let d = time_date.getDate();
+	let m = time_date.getMonth();
+	let y = time_date.getFullYear();
+
+	
+	
+	
+	
+
+	
+}
