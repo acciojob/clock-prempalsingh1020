e145bb504071c3bcf65f5504e249e1b3c0a7ad78
prempalsingh1020 <prempalsingh030499@gmail.com>
2026-03-15 15:24:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,18 +4,33 @@
 let para  = document.querySelector("#timer");
 
 
-let time_date = new Date();
 
 function  ClockTimer(){
 
+let time_date = new Date();
+	
 	let str = ""
 	let h = time_date.getHours();
-	let m = time_date.getMinutes();
+	let mi = time_date.getMinutes();
 	let s = time_date.getSeconds();
 
 	let d = time_date.getDate();
 	let m = time_date.getMonth();
 	let y = time_date.getFullYear();
+
+	// let d = d<10?"0":d;
+	 m = m<10?"0"+m:m;
+     mi = mi<10?"0"+mi:mi;
+	str = `${d}/${m}/${y}, ${h}:${mi}:${s}`;
+	para.innerText = str;
+
+	// /function call after 1 second;
+	setTimeout(ClockTimer(),1000);
+	
+}
+
+ClockTimer();
+	
 
 	
 	
